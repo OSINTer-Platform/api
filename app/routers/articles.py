82417623d206ae2e3470eae583591a7d625c89ee
bertmad3400 <skrivtilbertram@gmail.com>
2022-05-02 08:21:00 +0200

    Add endpoint for search for articles

--- app/routers/articles.py
+++ app/routers/articles.py
@@ -10,3 +10,7 @@ router = APIRouter(
 @router.get("/overview/newest")
 async def get_newest_articles():
     return config_options.esArticleClient.queryDocuments(searchQuery(limit = 50, complete = False), return_object = False)["documents"]
+
+@router.get("/overview/search")
+async def search_articles(query: searchQuery = Depends(searchQuery)):
+    return config_options.esArticleClient.queryDocuments(query, return_object = False)["documents"]
