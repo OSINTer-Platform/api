ebd2914ed2f9c9dfe9f82c08278f22231d506736
bertmad3400 <skrivtilbertram@gmail.com>
2022-05-05 18:03:20 +0200

    Split document route up

--- app/main.py
+++ app/main.py
@@ -1,32 +1,22 @@
 from fastapi import FastAPI, Depends, Request
-from .routers import documents
+from .routers.documents import articles, tweets
 
 from . import config_options
 
 from OSINTmodules.OSINTelastic import elasticDB
 
-# Wrapper class used to pass the right es client to each mount of the documents router using dependency injection and the request state
-class es_client_getter():
-    def __init__(self, es_client_name: str):
-        self.es_client = config_options[es_client_name]
-
-    def __call__(self, request : Request):
-        request.state.es_client = self.es_client
-
 app = FastAPI()
 
 app.include_router(
-        documents.router,
-        prefix = "/tweets",
-        tags=["tweets", "documents"],
-        dependencies = [Depends(es_client_getter("esTweetClient"))]
+        articles.router,
+        prefix = "/articles",
+        tags=["articles", "documents"]
     )
 
 app.include_router(
-        documents.router,
-        prefix = "/articles",
-        tags=["articles", "documents"],
-        dependencies = [Depends(es_client_getter("esArticleClient"))]
+        tweets.router,
+        prefix = "/tweets",
+        tags=["tweets", "documents"]
     )
 
 @app.get("/")
---
+++ app/routers/documents/articles.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends, Query
+from ... import config_options
+
+from OSINTmodules.OSINTelastic import searchQuery
+from ...dependencies import fastapiSearchQuery
+
+from pydantic import conlist, constr
+
+router = APIRouter()
+
+@router.get("/overview/newest")
+async def get_newest_articles():
+    return config_options.esArticleClient.queryDocuments(searchQuery(limit = 50, complete = False), return_object = False)["documents"]
+
+@router.get("/overview/search")
+async def search_articles(query: fastapiSearchQuery = Depends(fastapiSearchQuery)):
+    return config_options.esArticleClient.queryDocuments(query, return_object = False)["documents"]
+
+@router.get("/content")
+async def get_article_content(IDs: conlist(constr(strip_whitespace = True, min_length = 20, max_length = 20)) = Query(...)):
+    return config_options.esArticleClient.queryDocuments(searchQuery(IDs = IDs, complete = True))["documents"]
+
+@router.get("/categories")
+async def get_list_of_categories():
+    return config_options.esArticleClient.requestSourceCategoryListFromDB()
