911825be55956f00697754ac65ab7e62516de050
bertmad3400 <skrivtilbertram@gmail.com>
2022-05-07 10:52:18 +0200

    Remove global user dependency

--- app/routers/users/feeds.py
+++ app/routers/users/feeds.py
@@ -7,9 +7,7 @@ from ..auth import get_user_from_token
 from ...users import User, Feed
 from ...common import DefaultResponse, DefaultResponseStatus
 
-router = APIRouter(
-            dependencies=[Depends(get_user_from_token)]
-        )
+router = APIRouter()
 
 @router.get("/list", response_model = List[Feed])
 def get_my_feeds(current_user: User = Depends(get_user_from_token)):
