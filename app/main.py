300e44f8a1632b10f08d4cc83760d24deadec2f6
bertmad3400 <skrivtilbertram@gmail.com>
2022-05-19 21:21:47 +0200

    Add CORS middelware to allow for requests from port 8080 and 8000 for testing

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Depends, Request
+from fastapi.middleware.cors import CORSMiddleware
+
 from .routers.documents import articles, tweets
 from .routers.users import feeds
 from .routers import auth
@@ -8,6 +10,20 @@ from . import config_options
 from OSINTmodules.OSINTelastic import elasticDB
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
         articles.router,
