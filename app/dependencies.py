a45f2a23f7bde91435546543afe91c9c445e5e06
bertmad3400 <skrivtilbertram@gmail.com>
2022-08-09 10:58:32 +0200

    Add dependency function for getting collection IDs

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,10 +1,16 @@
 from modules.elastic import searchQuery
 
-from fastapi import Query
+from fastapi import Query, Path
 from datetime import datetime
 from typing import Optional
 from pydantic import conlist, constr
 from dataclasses import dataclass
+
+from fastapi import Depends, HTTPException, status
+from .routers.auth import get_user_from_token
+from .users import User
+from .common import HTTPError
+
 
 # Using wrapper around searchQuery class, to force fastapi to make all arguments query (and not body), by using Query([defaultArgument]) and not just [defaultargument]
 @dataclass
@@ -23,3 +29,17 @@ class fastapiSearchQuery(searchQuery):
     complete: bool = Query(
         False
     )  # For whether the query should only return the necessary information for creating an article object, or all data stored about the article
+
+
+def get_collection_IDs(
+    collection_name: str = Path(...), current_user: User = Depends(get_user_from_token)
+):
+    try:
+        collection_IDs = current_user.get_collections()[collection_name]
+    except KeyError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Found no collection with given name",
+        )
+
+    return collection_IDs
