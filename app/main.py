b34fe16fa0e858004072b14cef4074b0883f1a5d
bertmad3400 <skrivtilbertram@gmail.com>
2022-09-01 10:59:31 +0200

    Simplify tags used for different endpoints

--- app/main.py
+++ app/main.py
@@ -20,14 +20,14 @@ app = FastAPI(
     root_path="/api",
 )
 
-app.include_router(articles.router, prefix="/articles", tags=["articles", "documents"])
+app.include_router(articles.router, prefix="/articles", tags=["articles"])
 
-app.include_router(tweets.router, prefix="/tweets", tags=["tweets", "documents"])
+app.include_router(tweets.router, prefix="/tweets", tags=["tweets"])
 
 app.include_router(auth.router, prefix="/auth", tags=["authorization"])
 
-app.include_router(feeds.router, prefix="/users/feeds", tags=["users", "feed"])
+app.include_router(feeds.router, prefix="/users/feeds", tags=["feed"])
 
 app.include_router(
-    collections.router, prefix="/users/collections", tags=["users", "collections"]
+    collections.router, prefix="/users/collections", tags=["collections"]
 )
