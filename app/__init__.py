f4a135cb3deb69ef07537ea076b569d1e4155221
bertmad3400 <skrivtilbertram@gmail.com>
2023-01-03 08:59:42 +0100

    Use python-dotenv to load enviroment variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from modules.config import FrontendConfig, configure_logger
 from modules.misc import create_folder
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 create_folder("logs")
 configure_logger("osinter")
 
