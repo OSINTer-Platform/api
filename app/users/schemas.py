f430961d7f9d4927bd3c914aade5eae58e89b920
bertmad3400 <skrivtilbertram@gmail.com>
2023-02-13 15:18:02 +0100

    Add function for converting feed or collection to query

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -4,6 +4,8 @@ from uuid import UUID, uuid4
 from pydantic import BaseModel, Field
 from app.dependencies import ArticleSortBy, ArticleSortOrder
 
+from modules.elastic import SearchQuery
+
 
 # Used for mapping the _id field of the DB model to the schemas id field
 class ORMBase(BaseModel):
@@ -35,6 +37,22 @@ class FeedCreate(BaseModel):
     class Config:
         use_enum_values = True
 
+    def to_query(self):
+        return SearchQuery(
+            limit = self.limit if self.limit else 0,
+
+            sort_by=str(self.sort_by) if self.sort_by else None,
+            sort_order=str(self.sort_order) if self.sort_order else None,
+
+            search_term=self.search_term,
+            highlight=True if self.search_term and self.highlight else False,
+
+            first_date=self.first_date,
+            last_date=self.last_date,
+
+            source_category=self.source_category
+        )
+
 
 class Feed(ItemBase, FeedCreate):
     pass
@@ -42,6 +60,16 @@ class Feed(ItemBase, FeedCreate):
 
 class Collection(ItemBase):
     ids: set[str] = set()
+
+    def to_query(self):
+        return SearchQuery(
+            limit=10_000 if len(self.ids) < 10_000 else 0,
+
+            sort_by=ArticleSortBy.PublishDate.value,
+            sort_order=ArticleSortOrder.Descending.value,
+
+            ids=list(self.ids)
+        )
 
 
 class UserBase(ORMBase):
