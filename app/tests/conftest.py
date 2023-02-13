8f5a4ec293920d41abc4660777d9ddaba2a7aaaa
bertmad3400 <skrivtilbertram@gmail.com>
2023-02-13 08:33:15 +0100

    Add unit tests for collections

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,5 +1,7 @@
 from collections.abc import Callable
 from uuid import uuid4
+import string
+import random
 
 from pydantic_factories import ModelFactory
 import pytest
@@ -34,3 +36,26 @@ def get_feeds() -> Callable[[int], list[FeedCreate]]:
         return [FeedFactory.build() for i in range(n)]
 
     return _make_feed_list
+
+
+@pytest.fixture
+def get_collections() -> Callable[[int, int], list[list[str]]]:
+    def _make_collection_list(n: int = 1000, max_size: int = 100) -> list[list[str]]:
+        collection_list = []
+
+        for _ in range(n):
+            collection = []
+
+            for _ in range(random.randint(1, max_size)):
+                collection.append(
+                    "".join(
+                        random.choice(string.ascii_uppercase + string.digits)
+                        for _ in range(20)
+                    )
+                )
+
+            collection_list.append(collection)
+
+        return collection_list
+
+    return _make_collection_list
