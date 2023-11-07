9d49a9348f73602895ae41b15995e939ec52c3de
bertmad3400 <skrivtilbertram@gmail.com>
2023-11-07 15:48:06 +0000

    Create seperate setup script
    For setting up couchdb DB and schemas

--- app/__init__.py
+++ app/__init__.py
@@ -3,29 +3,12 @@ from dotenv import load_dotenv
 from modules.config import configure_logger
 from modules.misc import create_folder
 
-from .config import FrontendConfig
-
-from couchdb import Server
-from couchdb.http import PreconditionFailed
-
-
-def init_db(url: str, db_name: str) -> None:
-    couch = Server(url)
-
-    try:
-        couch.create(db_name)
-    except PreconditionFailed:
-        pass
-
-    return None
-
+from config import FrontendConfig
 
 load_dotenv()
 
 create_folder("logs")
 configure_logger("osinter")
 
-
-init_db(*FrontendConfig.get_couchdb_details())
 
 config_options = FrontendConfig()
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,26 +7,3 @@ max_requests_jitter = 50
 log_file = "-"
 
 workers = multiprocessing.cpu_count() * 2 + 1
-
-# App specific
-from app import config_options
-from app.config import FrontendConfig
-
-from app.users.models import views
-from app.users.standard import create_standard_items
-
-from couchdb.mapping import ViewDefinition
-
-
-def on_starting(_):
-    ViewDefinition.sync_many(config_options.couch_conn, views)
-    create_standard_items()
-
-
-def post_fork(_, __): # pyright: ignore
-    global config_options
-    config_options = FrontendConfig()
-
-
-if __name__ == "__main__":
-    on_starting(None)
