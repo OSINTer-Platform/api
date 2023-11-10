50bb45d8378c83d336b67739cfb8fd8ec46e591d
bertmad3400 <skrivtilbertram@gmail.com>
2023-11-10 11:23:18 +0000

    Return None instead of exception when missing auth token
    This is to allow routes to return different data depending on auth status

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -33,14 +33,7 @@ class OAuth2PasswordBearerWithCookie(OAuth2):
 
         scheme, param = get_authorization_scheme_param(authorization)
         if not authorization or scheme.lower() != "bearer":
-            if self.auto_error:
-                raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail="Not authenticated",
-                    headers={"WWW-Authenticate": "Bearer"},
-                )
-            else:
-                return None
+            return None
         return param
 
 
