ff8f76c6c1a3b95c6778b2e0d05d1a4daa211c72
bertmad3400 <skrivtilbertram@gmail.com>
2024-06-04 09:05:24 +0000

    Implement webhook connectors for discord and slakc

--- app/connectors/__init__.py
+++ app/connectors/__init__.py
@@ -1,3 +1,37 @@
-from typing import TypeAlias, Literal
+from typing import Callable, Coroutine, Generic, Literal, TypeAlias, TypeVar, TypedDict
+from discord import Embed
+
+from modules.objects.articles import BaseArticle
+from . import discord, slack
+
 
 WebhookType: TypeAlias = Literal["discord", "slack"]
+
+ConnectorInput = TypeVar("ConnectorInput")
+
+
+class Connector(TypedDict, Generic[ConnectorInput]):
+    format: Callable[[list[BaseArticle], str], ConnectorInput]
+    send_messages: Callable[
+        [list[tuple[list[str], ConnectorInput]]], Coroutine[None, None, None]
+    ]
+    validate: Callable[[str], Coroutine[None, None, bool]]
+
+
+class ConnectorOverview(TypedDict, total=True):
+    discord: Connector[list[list[Embed]]]
+    slack: Connector[list[list[slack.BlockMsg]]]
+
+
+connectors: ConnectorOverview = {
+    "discord": {
+        "format": discord.format,
+        "send_messages": discord.send_messages,
+        "validate": discord.validate,
+    },
+    "slack": {
+        "format": slack.format,
+        "send_messages": slack.send_messages,
+        "validate": slack.validate,
+    },
+}
---
+++ app/connectors/discord.py
@@ -0,0 +1,73 @@
+import asyncio
+from typing import Any, Coroutine
+from discord import Embed, Webhook
+import aiohttp
+
+from modules.objects import BaseArticle
+from app import config_options
+
+
+def format(articles: list[BaseArticle], feed_name: str) -> list[list[Embed]]:
+    def create_embed(article: BaseArticle) -> Embed:
+        embed = Embed(
+            title=article.title,
+            url=f"{config_options.ARTICLE_RENDER_URL}/{article.id}",
+            description=f"**{article.description}**",
+            colour=0xD4163C,
+            timestamp=article.publish_date,
+        )
+
+        embed.set_image(url=article.image_url)
+
+        embed.set_footer(text=f'From "{feed_name}"')
+
+        return embed
+
+    batches: list[list[Embed]] = [[]]
+    batch_length = 0
+
+    for article in articles:
+        new_embed = create_embed(article)
+        embed_length = len(new_embed)
+
+        if embed_length + batch_length > 6000 or len(batches[-1]) > 9:
+            batches.append([])
+            batch_length = 0
+
+        batches[-1].append(new_embed)
+        batch_length += embed_length
+
+    return batches
+
+
+async def send_messages(messages: list[tuple[list[str], list[list[Embed]]]]) -> None:
+    async with aiohttp.ClientSession() as session:
+        send_actions: list[Coroutine[Any, Any, None]] = []
+
+        for urls, embed_batches in messages:
+            try:
+                for url in urls:
+                    webhook = Webhook.from_url(url, session=session)
+                    for embeds in embed_batches:
+                        action = webhook.send(
+                            "",
+                            wait=False,
+                            embeds=embeds,
+                            username="OSINTer",
+                            avatar_url=config_options.SMALL_LOGO_URL,
+                        )
+                        send_actions.append(action)
+            except ValueError:
+                pass
+
+        await asyncio.gather(*send_actions, return_exceptions=True)
+
+
+async def validate(url: str) -> bool:
+    try:
+        async with aiohttp.ClientSession() as session:
+            webhook = Webhook.from_url(url, session=session)
+            await webhook.send("Initializing webhook from OSINTer...", silent=True)
+        return True
+    except:
+        return False
