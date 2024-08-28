7f7c2f0e854bd3d004d3441fb697bd4b63d7ceda
bertmad3400 <skrivtilbertram@gmail.com>
2024-08-28 13:53:04 +0000

    Show webhook limits in app stats endpoint

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,8 +1,8 @@
-from typing import TypedDict
+from typing import Literal, TypedDict
 from fastapi import APIRouter
 
 from app import config_options
-from app.authorization import Area, Level, levels_access
+from app.authorization import Area, Level, WebhookLimits, levels_access, webhook_limits
 
 router = APIRouter()
 
@@ -16,6 +16,7 @@ class MLStats(TypedDict):
 
 class AuthStats(TypedDict):
     allowed_areas: dict[Level, list[Area]]
+    webhook_limits: dict[Literal[Level, "premium"], WebhookLimits]
 
 
 class Stats(TypedDict):
@@ -32,5 +33,5 @@ def get_applications_stats() -> Stats:
             "elser": config_options.ELSER_AVAILABLE,
             "assistant": config_options.LIVE_INFERENCE_AVAILABLE,
         },
-        "auth": {"allowed_areas": levels_access},
+        "auth": {"allowed_areas": levels_access, "webhook_limits": webhook_limits},
     }
