3919acb737c0fd535931d30ea4a8761ca97418d8
bertmad3400 <skrivtilbertram@gmail.com>
2024-10-03 13:05:29 +0000

    Add endpoint for storing user address

--- app/routers/user/payment/__init__.py
+++ app/routers/user/payment/__init__.py
@@ -1,11 +1,18 @@
-from fastapi import APIRouter, Depends, HTTPException
+from typing import Annotated
+from fastapi import APIRouter, Body, Depends, HTTPException
 
-from starlette.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
+from starlette.status import (
+    HTTP_404_NOT_FOUND,
+    HTTP_422_UNPROCESSABLE_ENTITY,
+    HTTP_500_INTERNAL_SERVER_ERROR,
+)
 import stripe
 
 from app import config_options
 from app.users.auth import ensure_user_from_request
-from app.users.schemas import User
+from app.users.crud import update_user
+from app.users.schemas import User, UserAddress
+from app.users.stripe import get_or_create_customer
 from .subscriptions import router as subscription_router
 
 stripe.api_key = config_options.STRIPE_API_KEY
@@ -29,3 +36,41 @@ def get_action_payment_intent(
             HTTP_500_INTERNAL_SERVER_ERROR, "Error when loading payment intent"
         )
     return {"client_secret": payment_intent.client_secret}
+
+
+@router.put("/address")
+def update_customer_address(
+    user_and_customer: Annotated[
+        tuple[User, stripe.Customer], Depends(get_or_create_customer)
+    ],
+    address: Annotated[UserAddress, Body()],
+) -> User:
+    user, customer = user_and_customer
+
+    stripeAddress: stripe.Customer.ModifyParamsAddress = {
+        "city": address.city,
+        "country": address.country,
+        "line1": address.line1,
+        "state": address.state,
+        "postal_code": address.postal_code,
+    }
+
+    try:
+        customer.modify(
+            customer.id,
+            address=stripeAddress,
+            shipping={"name": address.customer_name, "address": stripeAddress},
+            tax={"validate_location": "immediately"},
+        )
+    except stripe.InvalidRequestError as e:
+        if e.code == "customer_tax_location_invalid":
+            raise HTTPException(
+                HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="Invalid address. Please specify a valid one",
+            )
+        raise e
+
+    user.payment.address = address
+    update_user(user)
+
+    return user
